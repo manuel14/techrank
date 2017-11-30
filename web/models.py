5f5ea12e93d9327ccef1c8b88c0428e9a5fa2e18
Manuel Zubieta <mzubieta@ushuaiavision.com.ar>
2017-11-30 11:18:45 -0300

    Prueba para hook de production

--- web/models.py
+++ web/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
 class Tecnico(models.Model):
     tecnico_id = models.CharField(max_length=10)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
